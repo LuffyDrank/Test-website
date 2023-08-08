f73793464f183210349e474c6b61a37a41d5aff2
LuffyDrank <luffydrank@gmail.com>
2023-08-08 15:57:22 -0400

    MOST BUTTONS WORK NOW
    The only thing left is the vehicles

--- assets/js/car_api.js
+++ assets/js/car_api.js
@@ -1,3 +1,401 @@
+/////////// NEW CODE ///// CHECKBOXES WORK WITH EVERY DRIVER START //////////////////////////
+
+let accidentsArray = [1];
+let ticketsArray = [1];
+let driversFullContainer = document.getElementById('Drivers-Full-Container');
+let driverCount = 1;
+
+driversFullContainer.addEventListener('click', doSomething, false);
+
+
+function getTicketNumber(driverNumber) {
+result = ticketsArray[driverNumber - 1];
+return result;
+}
+
+function getAccidentNumber(driverNumber) {
+result = accidentsArray[driverNumber - 1];
+return result;
+}
+
+function addTicketNumber(driverNumber) {
+  result = driverNumber - 1;
+  ticketArray = ticketsArray.splice(result, 1, ticketsArray[result] + 1);
+}
+
+
+function addAccidentNumber(driverNumber) {
+  result = driverNumber - 1;
+  accidentArray = accidentsArray.splice(result, 1, accidentsArray[result] + 1);
+}
+
+function subtractTicketNumber(driverNumber) {
+  result = driverNumber - 1;
+  ticketArray = ticketsArray.splice(result, 1, ticketsArray[result] - 1);
+}
+
+function subtractAccidentNumber(driverNumber) {
+  result = driverNumber - 1;
+  accidentArray = accidentsArray.splice(result, 1, accidentsArray[result] - 1);
+}
+
+
+function doSomething(e) {
+  if (e.target !== e.currentTarget) {
+    let clickedItem = e.target.id;
+
+
+
+    
+    // IF IT IS A TICKET CHECKBOX
+
+    if (clickedItem.endsWith('_Tickets_Checkbox')) {
+      let targetItem = document.getElementById(clickedItem);
+      let driverNumber = clickedItem.charAt(6);
+      let accidentLabel = document.getElementById(`Driver${driverNumber}_Accidents_Checkbox_Label`);
+      let ticketsFormFull = document.createElement('div');
+    
+     
+
+      ticketsFormFull.innerHTML = `
+      <div id="Driver${driverNumber}_Tickets_Form">
+      <label for="Driver${driverNumber}_Ticket_Type${getTicketNumber(driverNumber)}">${toWords(getTicketNumber(driverNumber))} Ticket Type</label>
+<select id="Driver${driverNumber}_Ticket_Type${getTicketNumber(driverNumber)}" name="Driver${driverNumber}_Ticket_Type${getTicketNumber(driverNumber)}">
+  <option value="">Choose an option if applicable...</option>
+  <option value="Speeding">Speeding</option>
+  <option value="Reckless driving">Reckless driving</option>
+  <option value="Driving under the influence of alcohol">Driving under the influence of alcohol</option>
+  <option value="Improper passing">Improper passing</option>
+  <option value="Following too closely">Following too closely</option>
+  <option value="Hit and run">Hit and run</option>
+  <option value="Improper turn">Improper turn</option>
+  <option value="Improper backing">Improper backing</option>
+  <option value="Texting while driving">Texting while driving</option>
+  <option value="Failure to yield">Failure to yield</option>
+  <option value="Tailgating">Tailgating</option>
+  <option value="Driving without insurance">Driving without insurance</option>
+  <option value="Failure to signal">Failure to signal</option>
+  <option value="Improper lane usage">Improper lane usage</option>
+  <option value="Driving on a suspended license">Driving on a suspended license</option>
+  <option value="Improper U-turn">Improper U-turn</option>
+  <option value="Driving on sidewalk">Driving on sidewalk</option>
+  <option value="Passing stopped school bus">Passing stopped school bus</option>
+  <option value="Failure to stop">Failure to stop</option>
+  <option value="Improper lane change">Improper lane change</option>
+  <option value="Illegal U-turn">Illegal U-turn</option>
+  <option value="Drinking while driving">Drinking while driving</option>
+</select>
+
+<div id="Driver${driverNumber}_Speeding_Options_Container${getTicketNumber(driverNumber)}" style="display: none;">
+  <label for="Driver${driverNumber}_Speeding_Options_${getTicketNumber(driverNumber)}">Speeding Options</label>
+  <select id="Driver${driverNumber}_Speeding_Options_${getTicketNumber(driverNumber)}" name="Driver${driverNumber}_Speeding_Options_${getTicketNumber(driverNumber)}">
+    <option value="">Choose an option...</option>
+    <option value="1-10 mph above the limit">1-10 mph above the limit</option>
+    <option value="11-14 mph above the limit">11-14 mph above the limit</option>
+    <option value="15-25 mph above the limit">15-25 mph above the limit</option>
+    <option value="More than 25mph above the limit">More than 25mph above the limit</option>
+  </select>
+</div>
+
+  <button type="button" class="add-remove-button" id="Driver${driverNumber}_Add_Ticket_Button">
+    + ADD TICKET
+  </button>
+
+  <button type="button" class="add-remove-button" id="Driver${driverNumber}_Remove_Ticket_Button">
+    - REMOVE TICKET
+  </button>
+`;
+
+
+
+      if (!accidentLabel.classList.contains('active')) {
+        accidentLabel.classList.add('active');
+        accidentLabel.before(ticketsFormFull);
+      }
+      else {
+        accidentLabel.classList.remove('active');
+        document.getElementById(`Driver${driverNumber}_Tickets_Form`).remove();
+
+      }
+    }
+
+    // IF IT IS A ACCIDENT CHECKBOX
+
+    if (clickedItem.endsWith('_Accidents_Checkbox')) {
+      let targetItem = document.getElementById(clickedItem);
+      let driverNumber = clickedItem.charAt(6);
+      let accidentCheckbox = document.getElementById(`Driver${driverNumber}_Accidents_Checkbox`);
+      let accidentsFormFull = document.createElement('div');
+
+      accidentsFormFull.innerHTML =
+       `
+       <div id="Driver${driverNumber}_Accidents_Form">
+      <label for="Driver${driverNumber}_Accident_Type_N${getAccidentNumber(driverNumber)}">${toWords(getAccidentNumber(driverNumber))} Accident Type</label>
+      <select id="Driver${driverNumber}_Accident_Type_N${getAccidentNumber(driverNumber)}" name="Driver${driverNumber}_Accident_Type_N${getAccidentNumber(driverNumber)}">
+        <option value="">Choose an option...</option>
+        <option value="Vehicle Parked">Vehicle Parked</option>
+        <option value="Hit & Run">Hit & Run</option>
+        <option value="At-fault">At-fault</option>
+      </select>
+
+      <label for="Driver${driverNumber}_Accident_Date_N${getAccidentNumber(driverNumber)}">${toWords(getAccidentNumber(driverNumber))} Accident Date</label>
+      <input type="date" id="Driver${driverNumber}_Accident_Date_N${getAccidentNumber(driverNumber)}" name="Driver${driverNumber}_Accident_Date_N${getAccidentNumber(driverNumber)}">
+    
+
+      <button href="#" class="add-remove-button" type="button" id="Driver${driverNumber}_Add_Accident_Button">
+         + ADD ACCIDENT
+      </button>
+
+      <button href="#" class="add-remove-button" type="button" id="Driver${driverNumber}_Remove_Accident_Button">
+       - REMOVE ACCIDENT
+      </button>
+      </div>
+      `;
+
+      if (!accidentCheckbox.classList.contains('active')) {
+        accidentCheckbox.classList.add('active');
+        accidentCheckbox.after(accidentsFormFull);
+      }
+
+      else {
+        accidentCheckbox.classList.remove('active');
+        document.getElementById(`Driver${driverNumber}_Accidents_Form`).remove();
+
+      }
+    }
+
+
+  //IF IT IS A ADD TICKET BUTTON
+  if (clickedItem.endsWith('_Add_Ticket_Button')) { 
+    driverNumber = Number(clickedItem.charAt(6));
+    addTicketNumber(driverNumber);
+    ticketsForm = document.createElement('div');
+    ticketNumber = ticketsArray[ driverNumber - 1];
+    
+    
+    
+    ticketsForm.setAttribute("id", `Driver${driverNumber}_Tickets${ticketNumber}_Form${ticketNumber}`);
+    ticketsForm.innerHTML = `
+      <label for="Driver${driverNumber}_Ticket_Type${ticketNumber}">${toWords(ticketNumber) } Ticket Type</label>
+<select id="Driver${driverNumber}_Ticket_Type${ticketNumber}" name="Driver${driverNumber}_Ticket_Type${ticketNumber}">
+  <option value="">Choose an option if applicable...</option>
+  <option value="Speeding">Speeding</option>
+  <option value="Reckless driving">Reckless driving</option>
+  <option value="Driving under the influence of alcohol">Driving under the influence of alcohol</option>
+  <option value="Improper passing">Improper passing</option>
+  <option value="Following too closely">Following too closely</option>
+  <option value="Hit and run">Hit and run</option>
+  <option value="Improper turn">Improper turn</option>
+  <option value="Improper backing">Improper backing</option>
+  <option value="Texting while driving">Texting while driving</option>
+  <option value="Failure to yield">Failure to yield</option>
+  <option value="Tailgating">Tailgating</option>
+  <option value="Driving without insurance">Driving without insurance</option>
+  <option value="Failure to signal">Failure to signal</option>
+  <option value="Improper lane usage">Improper lane usage</option>
+  <option value="Driving on a suspended license">Driving on a suspended license</option>
+  <option value="Improper U-turn">Improper U-turn</option>
+  <option value="Driving on sidewalk">Driving on sidewalk</option>
+  <option value="Passing stopped school bus">Passing stopped school bus</option>
+  <option value="Failure to stop">Failure to stop</option>
+  <option value="Improper lane change">Improper lane change</option>
+  <option value="Illegal U-turn">Illegal U-turn</option>
+  <option value="Drinking while driving">Drinking while driving</option>
+</select>
+
+<div id="Driver${driverNumber}_Speeding_Options_Container${ticketNumber}" style="display: none;">
+  <label for="Driver${driverNumber}_Speeding_Options_${ticketNumber}">Speeding Options</label>
+  <select id="Driver${driverNumber}_Speeding_Options_${ticketNumber}" name="Driver${driverNumber}_Speeding_Options_${getTicketNumber(driverNumber)}">
+    <option value="">Choose an option...</option>
+    <option value="1-10 mph above the limit">1-10 mph above the limit</option>
+    <option value="11-14 mph above the limit">11-14 mph above the limit</option>
+    <option value="15-25 mph above the limit">15-25 mph above the limit</option>
+    <option value="More than 25mph above the limit">More than 25mph above the limit</option>
+  </select>
+</div>
+
+
+`;
+
+let lastTicket = document.getElementById(`Driver${driverNumber}_Speeding_Options_Container${ticketNumber - 1}`);
+  console.log(lastTicket);
+lastTicket.after(ticketsForm);
+
+  console.log(driverNumber + " " + ticketNumber - 1)
+  
+
+}
+
+//IF IT IS A REMOVE TICKET BUTTON
+
+if (clickedItem.endsWith('_Remove_Ticket_Button')) { 
+    driverNumber = Number(clickedItem.charAt(6));
+    ticketsNumber = ticketsArray[ driverNumber - 1];
+    document.getElementById(`Driver${driverNumber}_Tickets${ticketsNumber}_Form${ticketsNumber}`).remove();
+    subtractTicketNumber(driverNumber);
+    console.log('clicked');
+  }
+
+
+ //// IF IT IS A ADD ACCIDENT BUTTON
+ if (clickedItem.endsWith('_Add_Accident_Button')) { 
+    driverNumber = Number(clickedItem.charAt(6));
+    addAccidentNumber(driverNumber);
+    accidentsForm = document.createElement('div');
+    accidentsNumber = accidentsArray[ driverNumber - 1];
+    
+    accidentsForm.setAttribute("id", `Driver${driverNumber}_Accident${accidentsNumber}_Form${accidentsNumber}`)
+    accidentsForm.innerHTML = `
+    <label for="Driver${driverNumber}_Accident_Type_N${accidentsNumber}" id="Driver${driverNumber}_Accident_Type_N${accidentsNumber}_Label">${toWords(accidentsNumber)} Accident Type
+                </label>
+                <select id="Driver${driverNumber}_Accident_Type_N${accidentsNumber}" name="Driver${driverNumber}_Accident_Type_N${accidentsNumber}">
+                  <option value="">Vehicle Parked</option>
+                  <option value="">Hit & Run</option>
+                  <option value="">At-fault</option>
+                </select>
+
+                <label for="Driver${driverNumber}_Accident_Date_N${accidentsNumber}" id="Driver${driverNumber}_Accident_Date_N${accidentsNumber}_Label">${toWords(accidentsNumber)} Accident
+                  Date</label>
+                <input type="date" id="Driver${driverNumber}_Accident_Date_N${accidentsNumber}" name="Driver${driverNumber}_Accident_Date_N${accidentsNumber}">
+
+    `;
+    console.log(driverNumber);
+    console.log(accidentsNumber - 1);
+    lastAccident = document.getElementById(`Driver${driverNumber}_Accident_Date_N${accidentsNumber - 1}`);
+    lastAccident.after(accidentsForm);
+    console.log(lastAccident);
+    
+  }
+
+
+  // IF IT IS A REMOVE ACCIDENT BUTTON
+
+  if (clickedItem.endsWith('_Remove_Accident_Button')) { 
+    driverNumber = Number(clickedItem.charAt(6));
+    accidentsNumber = accidentsArray[ driverNumber - 1];
+    document.getElementById(`Driver${driverNumber}_Accident${accidentsNumber}_Form${accidentsNumber}`).remove();
+    subtractAccidentNumber(driverNumber);
+  }
+
+
+
+  }
+  e.stopPropagation();
+}
+
+  
+
+  
+
+
+
+
+//////////  NEW CODE ///// CHECKBOXES WORK WITH EVERY DRIVER END ///////////////////////////
+
+
+
+
+
+
+///////////////////////////////// DRIVER BUTTON CODE START /////////////////////////////////
+
+
+
+
+
+document.getElementById('Add_Driver_Button').addEventListener('click', function (event) {
+  event.preventDefault();
+  addDriver();
+});
+
+document.getElementById('Remove_Driver_Button').addEventListener('click', function (event) {
+  event.preventDefault();
+  removeDriver();
+});
+
+function addDriver() {
+  // Increment driver count
+  driverCount++;
+  ticketsArray.push(1);
+  accidentsArray.push(1);
+
+  // Create a new div for the driver section
+  const newDriverDiv = document.createElement('div');
+  newDriverDiv.innerHTML = `
+<label for="Driver_${toWords(driverCount)}_Name">${toWords(driverCount)} Driver Name</label>
+<input type="text" id="Driver_${toWords(driverCount)}_Name" name="Driver_${toWords(driverCount)}_Name">
+
+<label for="Driver_${toWords(driverCount)}_Date_of_Birth">${toWords(driverCount)} Driver Date of Birth</label>
+<input type="date" id="Driver_${toWords(driverCount)}_Date_of_Birth" name="Driver_${toWords(driverCount)}_Date_of_Birth">
+
+<label for="Driver's_${toWords(driverCount)}_License_Number">${toWords(driverCount)} Driver's License Number</label>
+<input type="number" id="Driver_${toWords(driverCount)}_License_Number" name="Driver_${toWords(driverCount)}_License_Number">
+
+<div class="preference">
+  <p>Violations</p>
+
+  <label for="Driver${driverCount}_Tickets_Checkbox">
+    Tickets
+    <input type="checkbox" id="Driver${driverCount}_Tickets_Checkbox" name="Driver${driverCount}_Tickets_Checkbox" value="Yes" >
+  </label>
+
+  <label id="Driver${driverCount}_Accidents_Checkbox_Label" for="Driver${driverCount}_Accidents_Checkbox">
+    Accidents
+    <input type="checkbox" id="Driver${driverCount}_Accidents_Checkbox" name="Driver${driverCount}_Accidents_Checkbox">
+  </label>
+</div>
+
+`;
+
+  // Insert the new driver section after the last driver div in the container
+  const driversContainer = document.getElementById('drivers-container');
+  const lastDriverDiv = driversContainer.lastElementChild;
+  driversContainer.insertAdjacentElement('beforeend', newDriverDiv);
+}
+
+function removeDriver() {
+  if (driverCount > 1) {
+    accidentsArray.pop();
+    ticketsArray.pop();
+
+    // Find the last added driver section
+    const driversContainer = document.getElementById('drivers-container');
+    const lastDriverDivToRemove = driversContainer.lastElementChild;
+
+    // Remove the last driver section from the container
+    driversContainer.removeChild(lastDriverDivToRemove);
+
+    // Decrement driver count
+    driverCount--;
+  }
+}
+
+// Helper function to convert numbers to words with first letter capitalized (e.g., 1 to "One", 2 to "Two", etc.)
+function toWords(num) {
+  const words = ['Zero', 'First', 'Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh', 'Eighth', 'Ninth', 'Tenth'];
+  return words[num] || num.toString();
+}
+
+
+///////////////////////////////// DRIVER BUTTON CODE END //////////////////////////////////////
+
+
+
+/////////////////////////////////////    CUSTOM CODE     ///////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+// CAR API
+
+
 $(document).ready(
 function ()
 {
@@ -12,6 +410,11 @@ function ()
 
      //Optional: Pass sold_in_us:true to the setFilters method to show only US models. 
      carquery.setFilters( {sold_in_us:false} );
+
+
+
+
+
 
      //Optional: initialize the year, make, model, and trim drop downs by providing their element IDs
      carquery.initYearMakeModelTrim('car-years', 'car-makes', 'car-models', 'car-model-trims');
